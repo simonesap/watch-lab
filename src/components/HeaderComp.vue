92179b76089d7547df4af0ed2476ad970f7062d4
Simone Saputo <simonesaputo0@gmail.com>
2022-07-30 13:34:42 +0200

    add-scss

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div>
+
+    <div class="vh_100">
+
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'HeaderComp',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/style/sass/app.scss';
+
+</style>
---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div>
+
+    <HeaderComp/>
+
+  </div>
+
+</template>
+
+<script>
+import HeaderComp from "../HeaderComp.vue";
+
+export default {
+  name: 'HomePage',
+  components: {
+    HeaderComp
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../../assets/style/sass/app.scss';
+
+</style>
