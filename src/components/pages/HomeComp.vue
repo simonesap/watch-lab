13985ab2c4feb960fbcbe9181e428d8087d81f16
Simone Saputo <simonesaputo0@gmail.com>
2022-08-01 21:10:33 +0200

    start-footer

--- src/components/pages/HomeComp.vue
+++ src/components/pages/HomeComp.vue
@@ -112,7 +112,50 @@
         
       </div>
 
+      <div class="vh_80 mt-100 mb-56 d_flex">
+        <div class="w_50 h_100 pointer">
+          <img class="w_100 h_100" src="../../assets/img/1-f.png" alt="">
+        </div>
+        <div class="w_50 h_100 d_flex flex_wrap">
+            <div class="w_2 h_50">
+              <img class="w_100 h_100" src="../../assets/img/2-f.png" alt="">
+            </div>
+            <div class="w_2 h_50">
+              <img class="w_100 h_100" src="../../assets/img/3-f.png" alt="">
+            </div>
+            <div class="w_2 h_50">
+              <img class="w_100 h_100" src="../../assets/img/5-f.png" alt="">
+            </div>
+            <div class="w_2 h_50">
+              <img class="w_100 h_100" src="../../assets/img/4-f.png" alt="">
+            </div>  
+        </div>
+      </div>
+
     </main>
+
+    <footer class="container_main d_flex flex_column mb-56">
+      <div class="h_30 bg_footer">
+        <div class="text_center mb-56 text_white"> 
+            <h2 class="fs_3 text_center mt-32">Get in Touch</h2>
+            <h4 class="mt-56 text_uppercase text_center">DISCOVER MORE ABOUT NEWS, EXCLUSIVE EVENTS AND DISCOUNT</h4>
+            <div>
+              <button class="text_center mlr_auto mt-72 pt-16 pb-16 pl-64 pr-64 btn_gold fs_1_02" placeholder="type your email"></button>
+              <button class="mb-56 pt-16 pb-16 pl-64 pr-64 btn_gold fs_1_02">ok</button>
+            </div>
+        </div>
+      </div>
+      <div class="h_60 bg_black_footer">
+        <div><h2>WATCH/LAB</h2></div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
 
   </div>
 
@@ -165,5 +208,13 @@ export default {
 
 <style lang="scss">
 @import '../../assets/style/sass/app.scss';
+
+  .bg_footer{
+    background-image: url('../../assets/img/scott-webb-p-0lWFknspg-unsplash.jpg');
+    background-position: center;
+    background-size: cover;
+    filter: opacity(0.8);
+}
+// filter: sepia(50%);
 
 </style>
